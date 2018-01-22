ba61aa8520ed1dc5aa4abf93cf9669030b9319f7
Almas Myrzatay <almasmyrzatay@gmail.com>
2018-01-22 16:21:49 -0500

    Update demo.py

--- clarifai/demo.py
+++ clarifai/demo.py
@@ -5,7 +5,7 @@ import os
 
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
-app = ClarifaiApp(api_key='f7a91f12d5664eb4938cd583278e83f9')
+app = ClarifaiApp(api_key='yourKey')
 
 def clarifyFunc(files):
 	model = app.models.get("d02b4508df58432fbb84e800597b8959")
@@ -13,5 +13,5 @@ def clarifyFunc(files):
 	response = model.predict([image])
 	return str(response)
 
-fName = '/Users/almas/Documents/Research/demos/clarify/S06_e02_02_34.516#0_N_phoebe.jpg'
+fName = 'pathToImage'
 print clarifyFunc(fName)
